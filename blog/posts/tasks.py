b8c47cc79e49b1fff9689a6612946743ed9259e2
Reza Heydari <mrreza1394@gmail.com>
2024-09-01 07:56:42 +0330

    remove is_fake filter beacuse of custom manager

--- blog/posts/tasks.py
+++ blog/posts/tasks.py
@@ -8,9 +8,9 @@ from posts.models import Post, Review
 @app.task(name="update_avg_of_posts")
 def update_avg_of_post(post_pk):
     post = Post.objects.get(pk=post_pk)
-    avg_rate = Review.objects.filter(post=post, is_fake=False) \
+    avg_rate = Review.objects.filter(post=post) \
         .annotate().annotate(hour=TruncHour("created_at")).values('hour') \
         .annotate(avg_per_hour=Avg("rate")).order_by("hour").aggregate(avg_rate=Avg("avg_per_hour"))["avg_rate"]
     post.avg_rate = avg_rate
-    post.avg_user_count = Review.objects.filter(post=post, is_fake=False).count()
+    post.avg_user_count = Review.objects.filter(post=post).count()
     post.save()
